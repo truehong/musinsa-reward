78de813491670a13604349849f879fca9bbb1b3b
truehong <hhhhhongse@gmail.com>
2022-12-08 21:22:19 +0900

    Initial commit

---
+++ src/main/java/com/musinsa/demo/MusinsaRewardApplication.java
@@ -0,0 +1,13 @@
+package com.musinsa.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusinsaRewardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusinsaRewardApplication.class, args);
+    }
+
+}
